2e359616e95d66ed2233eb8cef3f03abacda3c37
Cemanuels <c.emanuel_ufc@outlook.com>
2019-06-04 17:21:42 -0300

    Consertado
    Seção áurea consertada

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from sympy import *
 from secao_aurea import *
 
-teste = secao_aurea(0.5, 1.2)
+x1, x2 = symbols('x1 x2')
+variables = [x1, x2]
 
-print(teste)
+function = 100*(x2 - x1**2)**2 + (1 - x1)**2 
+
+secao_aurea(0.1, 0.2, function)
+print(secao_aurea(0.1, 0.2, function))
--- secao_aurea.py
+++ secao_aurea.py
@@ -1,16 +1,12 @@
 from sympy import *
 
-x1, x2 = symbols('x1 x2')
-variables = [x1, x2]
-
-#Função de teste:
-function = 100*(x2 - x1**2)**2 + (1 - x1)**2
-
-grad_function = lambdify((x1, x2), derive_by_array(function ,(x1, x2)))
-
 def funcao_phi(t, function):
+	x1, x2 = symbols('x1 x2')
+	variables = [x1, x2]
 	#Ponto inicial
 	x_in = Matrix([0, 0])
+	#Gradiente
+	grad_function = lambdify((x1, x2), derive_by_array(function ,(x1, x2)))
 	#Direção de descida
 	d = -1 * Matrix(grad_function(x_in[0], x_in[1]))
 	return function.subs(list(zip(variables, x_in + t * d)))
@@ -22,7 +18,8 @@ def secao_aurea(epsilon, ro, function):
 	a = 0
 	s = ro
 	b = 2*ro
-	while (funcao_phi(s, function) < funcao_phi(b, function)):
+	while (funcao_phi(b, function) < funcao_phi(s, function)):
+		print(f'phi(s): {funcao_phi(s, function)}, phi(b): {funcao_phi(b, function)}')
 		a = s
 		s = b
 		b = 2*b		
