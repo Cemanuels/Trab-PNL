063ffb668916961df5f4c2e52f1a1ca76e28d15d
Cemanuels <c.emanuel_ufc@outlook.com>
2019-06-05 18:49:02 -0300

    Seç. áurea funcionando

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from sympy import *
 from secao_aurea import *
-
+from armijo import *
 x1, x2 = symbols('x1 x2')
 variables = [x1, x2]
+x_in = [0, 0]
+function = x1**2 - 8*x1 + 16
 
-function = 100*(x2 - x1**2)**2 + (1 - x1)**2 
-
-secao_aurea(0.1, 0.2, function)
-print(secao_aurea(0.1, 0.2, function))
+print(secao_aurea(0.5, 0.01, function))
--- secao_aurea.py
+++ secao_aurea.py
@@ -1,5 +1,4 @@
 from sympy import *
-
 def funcao_phi(t, function):
 	x1, x2 = symbols('x1 x2')
 	variables = [x1, x2]
@@ -9,20 +8,23 @@ def funcao_phi(t, function):
 	grad_function = lambdify((x1, x2), derive_by_array(function ,(x1, x2)))
 	#Direção de descida
 	d = -1 * Matrix(grad_function(x_in[0], x_in[1]))
+	print('d: ',d)
 	return function.subs(list(zip(variables, x_in + t * d)))
 
-def secao_aurea(epsilon, ro, function):
+def secao_aurea(ro, epsilon, function):
 	#Intervalo [a,b]
-	t1 = float((3 - sqrt(5))/2)
-	t2 = float(1 - t1)
+	t1 = (3.0 - sqrt(5.0))/2.0
+	t2 = 1.0 - t1
 	a = 0
 	s = ro
 	b = 2*ro
 	while (funcao_phi(b, function) < funcao_phi(s, function)):
-		print(f'phi(s): {funcao_phi(s, function)}, phi(b): {funcao_phi(b, function)}')
+		print(f'phi(b): {funcao_phi(b, function)}, phi(s): {funcao_phi(s, function)}')
 		a = s
+		print('a: ',a)
 		s = b
-		b = 2*b		
+		b = 2*b
+		print('b: ',b)		
 	#Obtenção do tamanho do passo:
 	u = a + t1*(b-a)
 	v = a + t2*(b-a)
